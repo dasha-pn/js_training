02f9e437116b5accdaf9835156c06ec3b92ac8bb
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-04-21 18:11:53 +0300

    added the 2 task

--- two_sum.js
+++ two_sum.js
@@ -1,32 +1,18 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
-    let carry = 0;
+var twoSum = function(nums, target) {
+    const map = new Map();
 
-    while (l1 !== null || l2 !== null || carry !== 0) {
-        let val1 = l1 !== null ? l1.val : 0;
-        let val2 = l2 !== null ? l2.val : 0;
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
 
-        let sum = val1 + val2 + carry;
-        carry = Math.floor(sum / 10);
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
 
-        current.next = new ListNode(sum % 10);
-        current = current.next;
-
-        if (l1 !== null) l1 = l1.next;
-        if (l2 !== null) l2 = l2.next;
+        map.set(nums[i], i);
     }
-    return dummy.next
 };
