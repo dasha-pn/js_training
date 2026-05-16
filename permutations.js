7ae75adf8e6b710444304c87344119cb80644b79
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-05-16 14:38:20 +0300

    added task 45

---
+++ permutations.js
@@ -0,0 +1,28 @@
+var permute = function(nums) {
+    let result = [];
+
+    function backtrack(path) {
+
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+
+        for (let num of nums) {
+
+            if (path.includes(num)) {
+                continue;
+            }
+
+            path.push(num);
+
+            backtrack(path);
+
+            path.pop();
+        }
+    }
+
+    backtrack([]);
+
+    return result;
+};
