dec798ec648874392a6944bcc8fa40376a18c71e
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-05-16 16:37:32 +0300

    added fixis

--- rotate_image.js
+++ rotate_image.js
@@ -10,3 +10,25 @@ var rotate = function(matrix) {
 };
 
 console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]));
+
+// Поворот на 90° складається з 2 кроків:
+
+// Транспонувати матрицю
+// Реверснути кожен рядок
+
+var rotate = function(matrix) {
+
+    // transpose
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix.length; j++) {
+
+            [matrix[i][j], matrix[j][i]] =
+            [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // reverse rows
+    for (let row of matrix) {
+        row.reverse();
+    }
+};
