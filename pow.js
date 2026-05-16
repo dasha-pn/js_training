14656b461483cd9c7e04de3cfab503ddfd268cf1
Daryna Shevchuk <d.shevchuk.pn@ucu.edu.ua>
2026-05-16 20:26:13 +0300

    added 50

---
+++ pow.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    let power = Math.abs(n);
+    let result = 1;
+
+    while (power > 0) {
+        if (power % 2 === 1) {
+            result *= x;
+        }
+
+        x *= x;
+        power = Math.floor(power / 2);
+    }
+
+    return n < 0 ? 1 / result : result;
+};
